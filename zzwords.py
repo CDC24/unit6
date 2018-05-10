b1518f27a862cbcddd9175d28ab80efc88043d23
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-10 08:18:20 -0400

    Updated from Brython Server: 10/05/2018 08:18:19

--- zzwords.py
+++ zzwords.py
@@ -1,11 +1,11 @@
 #Caleb Callaway
 #5/10/18
 #zzwords.py - print every word in dictionary with 'zz' in it
+
+
+
 file=open("engmix.txt")
 
-longest = ""
-
 for word in file:
-    if len(word) > len(longest):
-        longest = word
-print("The longest word is",longest)
+    if "zz" in word:
+        print (word)
