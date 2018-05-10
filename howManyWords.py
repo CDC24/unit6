62b9379f94c686d1eb2da9737075774272da8fdc
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-10 08:27:18 -0400

    Updated from Brython Server: 10/05/2018 08:27:17

--- howManyWords.py
+++ howManyWords.py
@@ -6,5 +6,31 @@
 
 file=open("engmix.txt")
 
-for word in file:
 
+1s = 0
+2s = 0
+3s = 0
+4s = 0
+5s = 0
+6s = 0
+7s = 0
+8s = 0
+9s = 0
+10s = 0
+11s = 0
+12s = 0
+13s = 0
+14s = 0
+15s = 0
+16s = 0
+17s = 0
+18s = 0
+19s = 0
+20s = 0
+21s = 0
+
+for word in file:
+    for i in range (0,21):
+        if len(word)==i:
+            (i,"s")+=1
+    
