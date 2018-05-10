5924ad33058546c01e34ebe770b45a9cfaef544f
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-10 08:32:01 -0400

    Updated from Brython Server: 10/05/2018 08:32:00

--- howManyWords.py
+++ howManyWords.py
@@ -7,30 +7,11 @@
 file=open("engmix.txt")
 
 
-1s = 0
-2s = 0
-3s = 0
-4s = 0
-5s = 0
-6s = 0
-7s = 0
-8s = 0
-9s = 0
-10s = 0
-11s = 0
-12s = 0
-13s = 0
-14s = 0
-15s = 0
-16s = 0
-17s = 0
-18s = 0
-19s = 0
-20s = 0
-21s = 0
+
+kindsawords=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 for word in file:
-    for i in range (0,21):
-        if len(word)==i:
-            (i,"s")+=1
+        if len(word) == i:
+            kindsawords[i-1] += 1
+
     
