e3d9b9233eaa9286370d1b8c0fb20838fbb6e03f
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-10 08:01:50 -0400

    Update fileDemo.py

--- fileDemo.py
+++ fileDemo.py
@@ -1,3 +1,12 @@
 #Caleb Callaway
 #5/9/18
 #fileDemo.py - how to read a file
+
+
+
+file=open("engmix.txt")
+
+
+for line in file:
+    if "cale" in line:
+        print (line.strip())   #removes extra spaces from \n in original file
