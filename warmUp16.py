ea653af9fcb15dcd29b3516f703d0340c1a3d83b
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-11 08:24:06 -0400

    Updated from Brython Server: 11/05/2018 08:24:05

--- warmUp16.py
+++ warmUp16.py
@@ -1,3 +1,15 @@
 #Caleb Callaway
 #5/11/18
 #warmUp16.py - prints all file words that start with your first initial and ends with your last.
+
+file=open("engmix.txt")
+
+
+for line in file:
+    if len(line)>=2:
+        list = line.split()
+        if list[0]=="c" and list[-1]=="c":
+            print (line.strip())
+    if len(line)<2:
+        if "c" in line:
+            print (line.strip())
