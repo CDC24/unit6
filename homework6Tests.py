f02dbb794fdd7ebb02abb278db861e6f9beedfad
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-15 08:26:48 -0400

    Updated from Brython Server: 15/05/2018 08:26:47

--- homework6Tests.py
+++ homework6Tests.py
@@ -6,7 +6,7 @@
 file=open("engmix.txt")
 
 
-
+                #part 1
 """
 word = input ("enter a word: ")
 
@@ -19,6 +19,8 @@ for line in file:
 if IN == False:
     print ("Oof!",word,"is NOT in the dictionary!")
 """
+
+                #part 2
 """
 L = []
 for line in file:
@@ -29,9 +31,39 @@ num = int(input("Enter the number of the word you want: "))
 print (L[num-1])
 """
 
-
-file = ("warmUp16.py")
+                #part 3
+"""
+file = open("warmUp16.py")
 
 for line in file:
     print (line.strip(),"!")
+"""
+
+                #part 4
+
+letter = input ("input the letter you want: ")
+greatest = (0)
+best = ("")
+
+
+for line in file:
+    line = line.strip()
+    if letter in line:
+        lettcount=0
+        for letter in line:
+            lettcount+=1
+    if lettcount>greatest:
+        greatest = lettcount
+        best = line
+print (best)
+
+
+
+
+
+
+
+
+
+
     
