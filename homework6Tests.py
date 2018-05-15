afc915ec17006844bd63973bdc74ad698bc2252a
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-15 08:09:31 -0400

    Updated from Brython Server: 15/05/2018 08:09:30

--- homework6Tests.py
+++ homework6Tests.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #5/15/18
 #homework6Tests.py - four homework programs
+
+
+file=open("engmix.txt")
+
+word = input ("enter a word: ")
+
+if word in file:
+    print ("Yes!",word,"is in the dictionary!")
