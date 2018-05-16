fbdf013a7e003e3989f7b2c50eace58c4ad9851b
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-16 08:32:45 -0400

    Updated from Brython Server: 16/05/2018 08:32:44

--- palindromes.py
+++ palindromes.py
@@ -2,3 +2,17 @@
 #5/16/18
 #homework6Tests.py - four homework programs
 
+file=open("engmix.txt")
+
+i=0
+for line in file:
+    while i <20:
+        line = line.strip()
+        L = []
+        if L[0] == L[-1]:
+            for letter in line:
+                L.append(letter)
+            print(L.reverse)
+            i+=1
+    
+    
