abd344edee2cdeb875376f7c9c59f1fc00191d9c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-17 08:12:17 -0400

    Updated from Brython Server: 17/05/2018 08:12:16

--- warmUp17.py
+++ warmUp17.py
@@ -1,3 +1,14 @@
 #Caleb Callaway
 #5/17/18
-#warmUp17.py- find all letters in dictionary with letters of your name
+#warmUp17.py- find all words in dictionary with letters of your name
+
+
+file = open("engmix.py")
+
+
+
+for line in file:
+    line = line.strip()
+    if "callaway" in line:
+        print line
+    
