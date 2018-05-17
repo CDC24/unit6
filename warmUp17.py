63718dcdde8a2639d973af91c1df3e33981765d4
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-17 08:22:36 -0400

    Updated from Brython Server: 17/05/2018 08:22:36

--- warmUp17.py
+++ warmUp17.py
@@ -3,12 +3,27 @@
 #warmUp17.py- find all words in dictionary with letters of your name
 
 
-file = open("engmix.py")
+file = open("engmix.txt")
 
 
 
 for line in file:
     line = line.strip()
-    if "callaway" in line:
-        print line
-    
+    if "c" in line:
+        if "a" in line:
+            if "l" in line:
+                if "w" in line:
+                    if "y" in line:
+                        print (line)
+
+print (" ")
+print (" ")
+
+for line in file:
+    line = line.strip()
+    if "c" in line:
+        if "a" in line:
+            if "l" in line:
+                if "e" in line:
+                    if "b" in line:
+                        print (line)
