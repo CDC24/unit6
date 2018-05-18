819a6e74cff15839635baa4613d57bfeb5aab66b
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-18 08:37:55 -0400

    Updated from Brython Server: 18/05/2018 08:37:54

--- longShort.py
+++ longShort.py
@@ -1,3 +1,18 @@
 #Caleb Callaway
 #5/17/18
 #longShort.py - finds the longest and shortest word for each letter
+
+file = open("engmix.txt")
+
+
+T = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+longestWord = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+
+for line in file:
+    line = line.strip()
+    if "a" in line:
+        if len(line)> len(longestWord[0]):
+            longestWord[0] = line
+print longestWord[0]
