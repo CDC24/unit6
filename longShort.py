0832dcec2324b027b0adb308b575b1ce0f34a3f4
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-18 08:48:39 -0400

    Updated from Brython Server: 18/05/2018 08:48:38

--- longShort.py
+++ longShort.py
@@ -7,8 +7,18 @@ file = open("engmix.txt")
 
 T = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 longestWord = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+shortestWord = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
+print ("longest:")
+for i in range(0,25):
+    for line in file:
+        line = line.strip()
+        if len(line)>0 and line[0] == T[i]:
+            if len(line)> len(longestWord[i]):
+                longestWord[i] = line
+    print (longestWord[i])
 
+print ("shortest (longer than 1 letter):")
 for i in range(0,25):
     for line in file:
         line = line.strip()
