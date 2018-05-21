b5f20c8d0f04c356e4ee190b24ce670d4822f28d
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-21 08:06:48 -0400

    Update quiz6.py

--- quiz6.py
+++ quiz6.py
@@ -1,3 +1,29 @@
 #Caleb Callaway
 #5/21/18
 #quiz6.py - quiz program
+
+#Caleb Callaway
+#5/21/18
+#quiz6.py - quiz program
+
+file = open ("engmix.txt")
+
+
+                                    #Program 1
+
+"""    
+lett = input("what letter do you want? ")
+
+for line in file:
+    if (line.strip()).count(lett) == 4:
+        print (line.strip())
+"""
+
+                                    #Program 2
+
+
+for line in file:
+    line = line.strip()
+    if line[0] == line[4] and line[0] == line[8]:
+        print (line)
+        break
